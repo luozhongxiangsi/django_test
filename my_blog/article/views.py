641dbe3bd254f1684e112cc8b95939965538b633
luozhongxiangsi <876217348@qq.com>
2020-04-14 22:46:21 +0800

    list ok

--- my_blog/article/views.py
+++ my_blog/article/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 # 导入 HttpResponse 模块
-from django.http import HttpResponse
+from .models import ArticlePost
 
 # 视图函数
+
+
 def article_list(request):
-    return HttpResponse("Hello World!")
+    articles = ArticlePost.objects.all()
+    context = {'articles': articles}
+    return render(request, 'article/list.html', context)
