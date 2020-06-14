1af520e974c390a67c6635c4797b76834a14c6cd
luozhongxiangsi <876217348@qq.com>
2020-06-14 13:36:25 +0800

    完成第一阶段

--- my_blog/article/models.py
+++ my_blog/article/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.urls import reverse
 
 
 # Create your models here.
@@ -20,6 +21,7 @@ class ArticlePost(models.Model):
 
     # 文章更新时间。参数 auto_now=True 指定每次数据更新时自动写入当前时间
     updated = models.DateTimeField(auto_now=True)
+    total_views = models.PositiveIntegerField(default=0)
     # 内部类 class Meta 用于给 model 定义元数据
 
     class Meta:
@@ -31,3 +33,6 @@ class ArticlePost(models.Model):
     def __str__(self):
         # return self.title 将文章标题返回
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('article:article_detail', args=[self.id])
--- my_blog/article/views.py
+++ my_blog/article/views.py
@@ -6,13 +6,35 @@ import markdown
 from .forms import ArticlePostForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse
-
+from django.core.paginator import Paginator
+from django.db.models import Q
+from comment.models import Comment
 
 # 视图函数
 
+
 def article_list(request):
-    articles = ArticlePost.objects.all()
-    context = {'articles': articles}
+    search = request.GET.get('search')
+    order = request.GET.get('order')
+    if search:
+        if order == 'total_views':
+            article_list = ArticlePost.objects.filter(Q(title__icontains=search)|
+                                                      Q(body__contains=search)
+                                                      ).order_by('-total_views')
+        else:
+            article_list = ArticlePost.objects.filter(Q(title__icontains=search)|
+                                                      Q(body__contains=search)
+                                                      )
+    else:
+        search = ''
+        if order == 'total_views':
+            article_list = ArticlePost.objects.all().order_by('-total_views')
+        else:
+            article_list = ArticlePost.objects.all()
+    paginator = Paginator(article_list, 2)
+    page = request.GET.get('page')
+    articles = paginator.get_page(page)
+    context = {'articles': articles, 'order': order}
     return render(request, 'article/list.html', context)
 
 
@@ -20,15 +42,20 @@ def article_list(request):
 def article_detail(request, id):
     # 取出相应的文章
     article = ArticlePost.objects.get(id=id)
+    article.total_views += 1
+    article.save(update_fields=['total_views'])
     # 将markdown语法渲染成html样式
-    article.body = markdown.markdown(article.body, extensions=[
-        # 包含 缩写、表格等常用扩展
-        'markdown.extensions.extra',
-        # 语法高亮扩展
-        'markdown.extensions.codehilite'
-    ])
+    md = markdown.Markdown(
+        extensions=[
+            'markdown.extensions.extra',
+            'markdown.extensions.codehilite',
+            'markdown.extensions.toc',
+        ]
+    )
+    article.body = md.convert(article.body)
+    comments = Comment.objects.filter(article=id)
     # 需要传递给模板的参数
-    context = {'article': article}
+    context = {'article': article, 'toc': md.toc, 'comments': comments}
     # 载入模板，并返回context对象
     return render(request, 'article/detail.html', context)
 
@@ -66,6 +93,8 @@ def article_update(request, id):
 
     # 获取需要修改的具体文章对象
     article = ArticlePost.objects.get(id=id)
+    if request.user != article.author:
+        return HttpResponse("抱歉，你无权修改这篇文章。")
     # 判断用户是否为 POST 提交表单数据
     if request.method == "POST":
         # 将提交的数据赋值到表单实例中
